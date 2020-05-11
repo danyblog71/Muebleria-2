b4534f11f4d3e470bed167944c03aff8dc1a809b
Daniel <danyblog71@gmail.com>
2020-05-10 20:31:46 -0500

    agregacion de navbar en el header

--- app/Http/Controllers/StoreControllers/StoreController.php
+++ app/Http/Controllers/StoreControllers/StoreController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\StoreControllers;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
@@ -14,7 +15,8 @@ class StoreController extends Controller
      */
     public function index()
     {
-        return view('store.index');
+        $categories = Category::all();
+        return view('store.index', compact('categories'));
     }
 
     public function showProducts($texto)
