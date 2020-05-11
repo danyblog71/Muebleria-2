dfadfcae493603506a6dad80298d2d9b21ea61dc
Daniel <danyblog71@gmail.com>
2020-05-10 23:17:28 -0500

    metodos Auth y creacion de middleware para Administrador

--- resources/views/layouts/menu_admin.blade.php
+++ resources/views/layouts/menu_admin.blade.php
@@ -4,9 +4,13 @@
 	<title></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+<nav class="navbar navbar-expand-lg sticky-top navbar-light" style="background-color: #FFE65C";>
+  <a class="navbar-brand" href="#">Muebleria</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" align="center" id="navbarNavDropdown">
+    <ul class="navbar-nav">
       <li class="nav-item ">
         <a class="nav-link" href=" {{ route('admin.products_index') }}">Mostrar Productos <span class="sr-only">(current)</span></a>
       </li>
@@ -16,13 +20,18 @@
       <li class="nav-item ">
         <a class="nav-link" href=" {{ route('admin.category_index',$texto='Ofertas')}}">Ofertas</a>
       </li>
+      @auth       
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('login')}}" onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">Logout</a>
+        </li> 
+      @endauth 
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-    </form>
   </div>
 </nav>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+  @csrf
+</form>
 <body>
 	@yield('contenido')
 </body>
